6c65784f44664fb624ed87c04087fe5445c9bea2
momo3159 <tommydsha@gmail.com>
2020-10-10 20:58:22 +0900

    Create Maps.go

---
+++ Maps.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Split(s, " ")
+	wordsDist := make(map[string]int)
+	
+	for _, word := range words {
+	  if _, ok := wordsDist[word]; !ok{
+	    wordsDist[word] = 1
+	  } else {
+	    wordsDist[word] += 1
+	  }
+	}
+	
+	return wordsDist
+}
+
+func main() {
+	wc.Test(WordCount)
+}
+
